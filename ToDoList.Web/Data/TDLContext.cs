dbd81a42b137823279354ad601b17c9c86d4e151
marmax13 <marmax13@gmail.com>
2023-04-01 10:46:38 +0600

    Контекст для соединения с базой данных Создадим в корне папку Data и в ней новый класс TDLContext

---
+++ ToDoList.Web/Data/TDLContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Web.Data;
+
+public class TDLContext : DbContext
+{
+    public TDLContext(DbContextOptions options) : base(options)
+    {
+    }
+
+}
+
